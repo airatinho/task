c3003538e52019010c99257a2e06fdba55c76e33
karim <123>
2021-10-04 23:09:20 +0300

    тестовое

--- test-task/zimad/task.sql
+++ test-task/zimad/task.sql
@@ -1,9 +1,8 @@
-select * from (SELECT
-                      date(event_time),
-                      user_id,
-                      avg(event_value) ARPDAU
-                      from test
-where event_name='purchase' or event_name='launch'
-group by date(event_time),user_id
-)
-where ARPDAU is not null
+SELECT val_sum ,uniq_users ,CAST(val_sum as  float)/CAST(uniq_users as  float) as ARPDAU
+from (select COUNT(DISTINCT user_id)  as uniq_users FROM test
+where event_name='launch') join (
+select sum(event_value) as val_sum from test
+where event_name='purchase')
+
+
+
