71bbf662c8a4ca5c5fa01a232d453f06161a7366
airatinho <49910605+airatinho@users.noreply.github.com>
2021-11-19 18:34:41 +0300

    Delete test-task/zimad directory

--- test-task/zimad/task.sql
+++
@@ -1,8 +0,0 @@
-SELECT val_sum ,uniq_users ,CAST(val_sum as  float)/CAST(uniq_users as  float) as ARPDAU
-from (select COUNT(DISTINCT user_id)  as uniq_users FROM test
-where event_name='launch') join (
-select sum(event_value) as val_sum from test
-where event_name='purchase')
-
-
-
